2a2606c456fd28056bbe462cbdae14fcb212b860
azbrun <ajzbrun@gmail.com>
2020-01-23 08:50:38 -0300

    conexion con arduino y lectura de datos ok

--- WPFAppPrueba/MainWindow.xaml.cs
+++ WPFAppPrueba/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO.Ports;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,9 @@ namespace WPFAppPrueba
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string buffer { get; set; }
+
+        private SerialPort serialPort = new SerialPort("COM6", 9600, Parity.None, 8, StopBits.One);
 
         public MainWindow()
         {
@@ -37,11 +41,54 @@ namespace WPFAppPrueba
             });
             // Start the timer 
             _timer.Start();
+
+
+            serialPort.ReadTimeout = 1000; //establezco el tiempo de espera cuando una operación de lectura no finaliza
+            serialPort.Open(); //abro una nueva conexión de puerto serie
+
+
+            readArduino();
+        }
+
+
+        private void readArduino()
+        {
+            if (serialPort.IsOpen) //chequeo que el puerto este abierto
+            {
+                try
+                {
+                    //print(serialPort.ReadLine().ToString());
+
+                    string actual_line = serialPort.ReadLine().ToString();
+                    //print(actual_line);
+
+                    string[] datos = actual_line.Split('|');
+
+                    lbl_temp_int.Text = datos[0] + " C°";
+                    lbl_humedad_int.Text = datos[1] + " %";
+                }
+                catch (Exception e)
+                {
+                    
+                }
+
+            }
         }
         
         public void updTimer()
         {
-            lbl_horario.Content = DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second;
+            string horas = DateTime.Now.Hour.ToString();
+            string minutos = DateTime.Now.Minute.ToString();
+            string segundos = DateTime.Now.Second.ToString();
+
+            if (horas.Length == 1)
+                horas = "0" + horas;
+            if (minutos.Length == 1)
+                minutos = "0" + minutos;
+            if (segundos.Length == 1)
+                segundos = "0" + segundos;
+
+            lbl_hours.Text = horas + ":" + minutos + ":" + segundos;
         }
 
     }
